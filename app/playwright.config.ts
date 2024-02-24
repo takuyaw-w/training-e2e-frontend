f174bea1c4b120f8941af6aa5d90ef45adfe7ca9
takuyaw-w <takuya.jamtb@gmail.com>
2024-02-24 21:55:33 +0900

    fix: update

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
     trace: 'on-first-retry',
 
     /* Only on CI systems run the tests headless */
-    headless: !!process.env.CI
+    headless: true
   },
 
   /* Configure projects for major browsers */
